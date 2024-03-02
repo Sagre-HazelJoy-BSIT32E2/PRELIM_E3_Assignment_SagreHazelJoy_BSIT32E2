827da5ae23d487118363fa98f5d89a10f5b14a9e
DeLeon-JianPaolo-BSIT32E2 <jipaodel@gmail.com>
2024-03-02 16:43:34 +0800

    Prelim Assignment

---
+++ PRELIM_E3_SagreHazelJoy_BSIT32E2/PRELIM_E3_SagreHazelJoy_BSIT32E2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E3_SagreHazelJoy_BSIT32E2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
